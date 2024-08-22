88bce2148a9333ea0a80df839a15886f06d1c143
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-08-22 21:30:01 +0300

    Create the navigation bar and navigation item

---
+++ app/src/main/kotlin/com/tonyGnk/thessBus/designSystem/mobile/components/navigation/bar/NavigationBar.kt
@@ -0,0 +1,83 @@
+package com.tonyGnk.thessBus.designSystem.mobile.components.navigation.bar
+
+import android.content.Context
+import android.os.Build
+import android.view.RoundedCorner
+import android.view.WindowManager
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.tonyGnk.thessBus.designSystem.mobile.R
+import com.tonyGnk.thessBus.designSystem.mobile.appStyles.AppColor
+import com.tonyGnk.thessBus.designSystem.mobile.components.navigation.item.NavigationBarItem
+import androidx.compose.material3.NavigationBar as MaterialNavigationBar
+
+@Composable
+internal fun NavigationBar(enabledItems: Triple<Boolean, Boolean, Boolean>) {
+    val selected = remember { mutableIntStateOf(0) }
+    val topCorners = findTheRightCorners(LocalView.current.context)
+
+    MaterialNavigationBar(
+        modifier = Modifier.clip(RoundedCornerShape(topStart = topCorners, topEnd = topCorners)),
+        containerColor = AppColor.surfaceContainerLowest,
+    ) {
+        if (enabledItems.first) NavigationBarItem(
+            itemLabel = stringResource(R.string.destination_home),
+            isSelected = selected.intValue == 0,
+            onItemClick = { selected.intValue = 0 },
+            animationResource = R.raw.location_pin,
+            modifier = Modifier.weight(1f),
+            radius = topCorners
+        )
+        if (enabledItems.second) NavigationBarItem(
+            itemLabel = stringResource(R.string.destination_lines),
+            isSelected = selected.intValue == 1,
+            onItemClick = { selected.intValue = 1 },
+            animationResource = R.raw.lines,
+            modifier = Modifier.weight(1f),
+            radius = topCorners
+        )
+
+        if (enabledItems.third) NavigationBarItem(
+            itemLabel = stringResource(R.string.destination_alarms),
+            isSelected = selected.intValue == 2,
+            onItemClick = { selected.intValue = 2 },
+            animationResource = R.raw.alarm_clock_second,
+            modifier = Modifier.weight(1f),
+            radius = topCorners
+        )
+    }
+}
+
+
+@Composable
+fun findTheRightCorners(context: Context): Dp {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val insets = windowManager.currentWindowMetrics.windowInsets
+        val bottomLeft = insets.getRoundedCorner(RoundedCorner.POSITION_BOTTOM_LEFT)
+
+        if (bottomLeft != null) return getCornerSizeFromRadius(bottomLeft.radius)
+    }
+    return 0.dp
+}
+
+@Composable
+fun pxToDp(px: Int): Dp {
+    val density = LocalDensity.current.density
+    return (px / density).dp
+}
+
+@Composable
+fun getCornerSizeFromRadius(radiusInPx: Int): Dp {
+    val radiusInDp = pxToDp(radiusInPx)
+    return radiusInDp
+}
---
+++ app/src/main/kotlin/com/tonyGnk/thessBus/designSystem/mobile/components/navigation/item/Label.kt
@@ -0,0 +1,103 @@
+package com.tonyGnk.thessBus.designSystem.mobile.components.navigation.item
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.lerp
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import com.tonyGnk.thessBus.designSystem.mobile.appStyles.AppColor
+import com.tonyGnk.thessBus.designSystem.mobile.components.text.Text
+import com.tonyGnk.thessBus.designSystem.mobile.theme.ClpTheme
+
+private const val NAVIGATION_ITEM_FIXED_LABEL_SIZE = 13
+
+@Composable
+fun NavigationBarItemLabel(
+    modifier: Modifier = Modifier,
+    label: String = "Label",
+    color: Color = AppColor.onBackground,
+    selected: Boolean,
+) {
+    val textStyle = if (selected) {
+        LocalTextStyle.current.copy(
+            fontSize = NAVIGATION_ITEM_FIXED_LABEL_SIZE.dp.toSp(),
+            lineHeight = NAVIGATION_ITEM_FIXED_LABEL_SIZE.dp.toSp(),
+            fontWeight = FontWeight.Black
+        )
+    } else {
+        LocalTextStyle.current.copy(
+            fontSize = NAVIGATION_ITEM_FIXED_LABEL_SIZE.dp.toSp(),
+            lineHeight = NAVIGATION_ITEM_FIXED_LABEL_SIZE.dp.toSp(),
+            fontWeight = FontWeight.ExtraLight
+        )
+    }
+
+    val animatedTextStyle by animateTextStyleAsState(
+        targetValue = textStyle,
+        spring(stiffness = Spring.StiffnessLow)
+    )
+
+    Text(
+        text = label,
+        size = TextUnit.Unspecified, // This prevents scaling based on system settings
+        style = animatedTextStyle,
+        color = color,
+        modifier = modifier
+    )
+}
+
+
+@Composable
+fun animateTextStyleAsState(
+    targetValue: TextStyle,
+    animationSpec: AnimationSpec<Float> = spring(),
+    finishedListener: ((TextStyle) -> Unit)? = null
+): State<TextStyle> {
+
+    val animation = remember { Animatable(0f) }
+    var previousTextStyle by remember { mutableStateOf(targetValue) }
+    var nextTextStyle by remember { mutableStateOf(targetValue) }
+
+    val textStyleState = remember(animation.value) {
+        derivedStateOf {
+            lerp(previousTextStyle, nextTextStyle, animation.value)
+        }
+    }
+
+    LaunchedEffect(targetValue, animationSpec) {
+        previousTextStyle = textStyleState.value
+        nextTextStyle = targetValue
+        animation.snapTo(0f)
+        animation.animateTo(1f, animationSpec)
+        finishedListener?.invoke(textStyleState.value)
+    }
+
+    return textStyleState
+}
+
+@Composable
+fun Dp.toSp() = with(LocalDensity.current) { toSp() }
+
+@Composable
+@Preview
+private fun Preview() = ClpTheme {
+    NavigationBarItemLabel(selected = false)
+}
