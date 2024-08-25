ef69999d9cb43c83341aea803c08b44396ff2172
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-08-25 16:52:43 +0300

    New icons

--- app/src/main/kotlin/com/tonyGnk/thessBus/designSystem/mobile/appStyles/AppIcon.kt
+++ app/src/main/kotlin/com/tonyGnk/thessBus/designSystem/mobile/appStyles/AppIcon.kt
@@ -1,31 +1,11 @@
 package com.tonyGnk.thessBus.designSystem.mobile.appStyles
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.rounded.Check
-import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material.icons.rounded.Person
-import androidx.compose.material.icons.rounded.Search
 import com.tonyGnk.thessBus.designSystem.mobile.R
 
 object AppIcon {
-    val Add = R.drawable.add
-    val arrow_right = R.drawable.arrow_right
+    val gpsOn = R.drawable.location_crosshairs
+    val gpsOff = R.drawable.location_crosshairs_slash
 
-    val Check = Icons.Rounded.Check
-    val Close = Icons.Rounded.Close
-
-    val MoreVert = Icons.Default.MoreVert
-    val Person = Icons.Rounded.Person
-    val Search = Icons.Rounded.Search
-
-    val BackIcon = R.drawable.back
-
-    val tableFocus = R.drawable.table_focus
-    val table = R.drawable.table
-    val rulesFocus = R.drawable.rules_focus
-    val rules = R.drawable.rules
-    val workersFocus = R.drawable.workers_focus
-    val workersOutLined = R.drawable.workers
-
+    val back = R.drawable.back
+    val search = R.drawable.search
 }
