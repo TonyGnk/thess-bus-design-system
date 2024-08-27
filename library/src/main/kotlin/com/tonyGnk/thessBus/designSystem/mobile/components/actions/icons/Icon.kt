e7d462e4e668a1ad19f9489f9a75f50a49c57feb
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-08-27 18:45:54 +0300

    Lottie icons support and static icons

--- library/src/main/kotlin/com/tonyGnk/thessBus/designSystem/mobile/components/actions/icons/Icon.kt
+++ library/src/main/kotlin/com/tonyGnk/thessBus/designSystem/mobile/components/actions/icons/Icon.kt
@@ -1,4 +1,4 @@
-package com.tonyGnk.thessBus.designSystem.mobile.components.icon
+package com.tonyGnk.thessBus.designSystem.mobile.components.actions.icons
 
 
 import androidx.annotation.DrawableRes
@@ -11,16 +11,17 @@ import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
 import com.tonyGnk.thessBus.designSystem.mobile.R
 import com.tonyGnk.thessBus.designSystem.mobile.appStyles.AppPreview
+import com.tonyGnk.thessBus.designSystem.mobile.theme.ClpTheme
 import androidx.compose.material3.Icon as MaterialIcon
 
 @Composable
 fun Icon(
     modifier: Modifier = Modifier,
-    @DrawableRes iconRes: Int,
+    @DrawableRes iconRes: Int = 0,
     contentDescription: String = "",
     color: Color = LocalContentColor.current
 ) {
-    MaterialIcon(
+    if (iconRes != 0) MaterialIcon(
         painter = painterResource(iconRes),
         tint = color,
         contentDescription = contentDescription,
@@ -30,22 +31,22 @@ fun Icon(
 
 @Composable
 fun Icon(
-    imageVector: ImageVector,
     modifier: Modifier = Modifier,
+    imageVector: ImageVector? = null,
     contentDescription: String = "",
-    tint: Color = LocalContentColor.current
+    color: Color = LocalContentColor.current
 ) {
-    MaterialIcon(
+    if (imageVector != null) MaterialIcon(
         painter = rememberVectorPainter(imageVector),
         contentDescription = contentDescription,
         modifier = modifier,
-        tint = tint
+        tint = color
     )
 }
 
 @Composable
 @AppPreview.Brightness
-fun IconPreview() {
+private fun IconPreview() = ClpTheme {
     Icon(
         iconRes = R.drawable.search
     )
