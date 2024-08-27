08fede41baa660be4fae8d4b21781c6d0f7bff9e
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-08-27 13:58:10 +0300

    Showcase: New activity with splash screen support

---
+++ showCase/src/main/kotlin/com/tonyGnk/thessBus/designSystem/mobile/showCase/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.tonyGnk.thessBus.designSystem.mobile.showCase
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.DisposableEffect
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.rememberNavController
+import com.tonyGnk.thessBus.designSystem.mobile.showCase.navigation.MyNavHost
+import com.tonyGnk.thessBus.designSystem.mobile.theme.ClpTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+internal class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+
+
+        var keepSplashScreenOn = true
+        splashScreen.setKeepOnScreenCondition { keepSplashScreenOn }
+        lifecycleScope.launch {
+            delay(1)
+            keepSplashScreenOn = false
+        }
+
+        enableEdgeToEdge()
+        setContent {
+            val darkTheme = isSystemInDarkTheme()
+            DisposableEffect(darkTheme) {
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.auto(
+                        Color.TRANSPARENT,
+                        Color.TRANSPARENT,
+                    ) { darkTheme },
+                    navigationBarStyle = SystemBarStyle.auto(
+                        lightScrim,
+                        darkScrim,
+                    ) { darkTheme },
+                )
+                onDispose {}
+            }
+
+            ClpTheme {
+                val navController = rememberNavController()
+                MyNavHost(navController)
+            }
+        }
+    }
+}
+
+private val lightScrim = Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
+private val darkScrim = Color.argb(0x80, 0x1b, 0x1b, 0x1b)
