46d186912ecb63e39a65c366ebaf8a8d09b06544
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-11-08 15:49:06 +0200

    Remove old pbf files

--- showCaseMobile/build.gradle.kts
+++ showCaseMobile/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.tonyGnk.thessBus.designSystem.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
+        versionCode = 20
         versionName = "0.0.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
